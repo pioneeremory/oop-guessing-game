40ad1a3c8d4d21d48537371e75eab0d33ed320d4
logan <blacksuitphoto@gmail.com>
2025-11-15 14:26:38 -0800

    oop-final-saturday

--- guessing_game.py
+++ guessing_game.py
@@ -7,27 +7,27 @@ class GuessingGame():
     def __str__(self):
         return (f'this is my game and the answer is {self.answer}')
 
-    def guess(self, user_guess):
+    def guess(self, last_guess):
         # could also apply ternary operator here:
         # print('high' if user_guess > GuessingGame.answer 
         #       else 'low' if user_guess < GuessingGame.answer
         #       else 'correct')
-        if user_guess > self.answer:
+        if last_guess > self.answer:
             print("Too high")
-        elif user_guess < self.answer:
+        elif last_guess < self.answer:
             print("Too low")
         else:
             print("WINNER")
     def solved(self, last_guess):
+
         if last_guess == self.answer:
             return True
         else:
             return False
     
-
-game = GuessingGame(10)
+game = GuessingGame(random.randint(1,100))
 last_guess  = 0
-last_result = game.solved(last_guess)
+last_result = game.guess(last_guess)
 count = 0
 
 print("Hello, and welcome to the secret number Guessing Game")
@@ -42,6 +42,7 @@ while game.solved(last_guess) == False:
     last_guess  = int(input("Enter your guess: "))
     last_result == game.guess(last_guess)
     count += 1
+    print(f"You've made {count} wrong guesses")
 print(f"You guessed {last_guess}, that's correct!")
   
 
